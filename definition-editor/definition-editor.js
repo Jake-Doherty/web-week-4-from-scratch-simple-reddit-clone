d1bb9e80669d988b835888fdbf88631c0a2e85e9
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-10-05 21:20:40 -0700

    add image input listener to change photo preview

--- definition-editor/definition-editor.js
+++ definition-editor/definition-editor.js
@@ -3,9 +3,21 @@
 import '../auth/user.js';
 
 /* Get DOM Elements */
+const addDefinitionForm = document.getElementById('add-definition-form');
+const errorDispay = document.getElementById('error');
+const imageInput = document.getElementById('image-input');
+const imagePreview = document.getElementById('preview-image');
 
 /* State */
+let error = null;
 
 /* Events */
-
+imageInput.addEventListener('change', () => {
+    const file = imageInput.files[0];
+    if (file) {
+        imagePreview.src = URL.createObjectURL(file);
+    } else {
+        imagePreview.src = '/assets/placeholder-image.png';
+    }
+});
 /* Display Functions */
