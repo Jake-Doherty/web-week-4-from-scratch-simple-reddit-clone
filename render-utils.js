9b9a1aa5f7af8fbf9b037d2ca1bd5da98bf7a58f
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-10-06 10:45:38 -0700

    add render function

---
+++ render-utils.js
@@ -0,0 +1,34 @@
+export function renderDefinition(definition) {
+    const li = document.createElement('li');
+    li.classList.add('definition-item');
+
+    const a = document.createElement('a');
+    a.classList.add('definition-link');
+
+    const div = document.createElement('div');
+    div.classList.add('definition-div');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('definition-title');
+    h3.textContent = definition.subject;
+
+    const span = document.createElement('span');
+    span.classList.add('category');
+    span.textContent = definition.category;
+
+    const p = document.createElement('p');
+    p.classList.add('description');
+    p.textContent = definition.description;
+
+    const img = document.createElement('img');
+    img.classList.add('post-image');
+    img.src = definition.image_url;
+
+    div.append(h3, span, p, img);
+
+    a.append(div);
+
+    li.append(a);
+
+    return li;
+}
